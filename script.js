3ae8624f72d6672f61036a7c5bb269937418f897
bizzaro-mark <bizzaro.kredit@gmail.com>
2021-06-28 21:52:18 +0200

    last commit

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 $(function () {
+    
+  function change_page(){
+    window.location.href = "contact.html";
+  } 
+    const inViewport = (entries, observer) => {
+        entries.forEach(entry => {
+          entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+        });
+      };
+      
+      const Obs = new IntersectionObserver(inViewport);
+      const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+      
+      // Attach observer to every [data-inviewport] element:
+      const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+      ELs_inViewport.forEach(EL => {
+        Obs.observe(EL, obsOptions);
+      });
+
+
     let open = false;
-
-
     const rotate = (deg, ele) => {
         $(ele).css({ WebkitTransform: 'rotate(' + deg + 'deg)' }).css('transition', 'ease-in-out').css('transition', '0.3s');
         $(ele).css({ '-moz-transform': 'rotate(' + deg + 'deg)' }).css('transition', 'ease-in-out').css('transition', '0.3s');
@@ -25,4 +43,5 @@ $(function () {
 
     });
 
+    
 });
